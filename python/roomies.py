e1d164362581f7bded8215a9415c013c498fd3e4
fonorobert <fonorobert@gmail.com>
2014-12-01 11:46:44 +0100

    added type and 'emptyness' checking for expense dict fields eg. if payer is not str or amount is not int or float

--- python/roomies.py
+++ python/roomies.py
@@ -1,15 +1,14 @@
 class Balance():
-    empty = ({}, [], (), None, False, 0, "")
+
     def __init__(self, data):
         self.data = data
         self.balance = {}
 
     def _calculate(self, data):
-        # If ran for first time, set up self.balance dictionary
-        if self.balance in self.empty:
-            people = self._list_ppl(data)
-            for p in people:
-                self.balance[p] = 0
+        # Set up self.balance dictionary with new people
+        people = self._list_ppl(data)
+        for p in people:
+            self.balance[p] = 0
 
         #Main calculation loop
         for expense in data:
@@ -17,7 +16,17 @@ class Balance():
             payer = expense['payer']
             owner = expense['owner']
 
-            if owner in self.empty:
+            # If an empty payer is specified, this expense will be ignored
+            if self.empty(payer) or self.empty(amnt):
+                continue
+
+            #If invalid type is given for amnt, this expense will be ignored
+            #Can be changed to raise TypeError instead
+            if not isinstance(amnt, (int, float)):
+                #raise TypeError
+                continue
+
+            if self.empty(owner):
                 for person in self.balance.keys():
                     self.balance[person] -= amnt/len(self.balance)
                 self.balance[payer] += (amnt/len(self.balance))*2
@@ -27,12 +36,23 @@ class Balance():
 
     def _list_ppl(self, rawdata):
         people = []
+
         for record in rawdata:
-            if record['payer'] not in people:
+            if record['owner'] not in people and not self.empty(record['owner']):
+                people.append(record['owner'])
+            if record['payer'] not in people and not self.empty(record['payer']):
                 people.append(record['payer'])
             else:
                 continue
         return people
+
+    # Helper method to decide if input is falsey. Returns true if it is
+    def empty(self, input):
+            empty = ({}, [], (), None, False, 0, "")
+            if input in empty:
+                return True
+            else:
+                return False
 
     def getbalance(self):
         self._calculate(self.data)
--- python/test.py
+++ python/test.py
@@ -1,16 +1,35 @@
 import unittest
 from roomies import Balance
 
+
 class TestRoomies(unittest.TestCase):
 
     def setUp(self):
-        self.expenses = [{"payer": "John", "owner": "Jill", "amount": 300}, {"payer": "Jill", "owner": "", "amount": 130}]
+        self.expenses = []
+        self.expenses.append({"payer": "John", "owner": "Jill", "amount": 300})
+        self.expenses.append({"payer": "Jill", "owner": "", "amount": 120})
         self.expenses.append({"payer": "John", "owner": "Joe", "amount": 900})
-        self.b = Balance(self.expenses)
 
     def test_listing(self):
-        b = self.b
-        self.assertEqual(b._list_ppl(self.expenses), ["John", "Jill", "Joe"])
+        b = Balance(self.expenses)
+        exp_list = ["John", "Jill", "Joe"]
+        self.assertCountEqual(b._list_ppl(self.expenses), exp_list)
+
+    def test_balance_basic(self):
+        b = Balance(self.expenses)
+        exp_balace = {"John": 1160, "Jill": -260, "Joe": -940}
+        self.assertEqual(b.getbalance(), exp_balace)
+
+    def test_balance_invalid_input(self):
+        expenses = self.expenses
+        expenses.append({"payer": "", "owner": "Jill", "amount": 200})
+        expenses.append({"payer": "John", "owner": "Jill", "amount": 0})
+        expenses.append({"payer": "Jill", "owner": "John", "amount": "300"})
+
+        b = Balance(expenses)
+        exp_balace = {"John": 1160, "Jill": -260, "Joe": -940}
+        #self.assertRaises(TypeError, b.getbalance)
+        self.assertEqual(b.getbalance(), exp_balace)
 
 
 if __name__ == '__main__':
